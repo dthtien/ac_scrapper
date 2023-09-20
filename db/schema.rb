d56942519149462242ac1404a0b0f9cf826d205c
Dau The Tien <tiendt2311@gmail.com>
2023-09-20 11:37:49 +1000

    feature(items): add image url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_122617) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_013250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_122617) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "original_price_details"
+    t.text "image_url"
   end
 
 end
